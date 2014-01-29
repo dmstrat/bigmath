c3d7bf49f605e354e2358e9f9cbf9a7d9e4c1afa
dmorris <dmorris@141c7bf7-4fae-6d00-1aaa-2b2b6e855cfb>
2014-01-29 04:46:56 +0000

    added bigmathnumber class as the base class to do the math against.
    Added tests as well for most, if not all the work.

---
+++ Tests/bigmath.Tests.Unit/BigMathNumberTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using bigmath;
+
+namespace bigmath.Tests.Unit
+{
+  [TestClass]
+  public class BigMathNumberTests
+  {
+    [TestMethod]
+    public void AssertBigMathNumberExists()
+    {
+      var testItem = new BigMathNumber();
+      Assert.IsInstanceOfType(testItem, typeof(BigMathNumber));
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberAssignNumberFromInt()
+    {
+      var testItem = new BigMathNumber();
+      testItem = 10;
+      Assert.IsTrue(testItem.Equals(10));
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberAssignNumberFromAnotherBigMathNumber()
+    {
+      var testItem = new BigMathNumber();
+      testItem = 10;
+      var testItem2 = new BigMathNumber();
+      testItem2 = 20;
+      testItem = testItem2;
+      Assert.AreEqual(testItem, testItem2);
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberInstantiatesWithZeroValueByEqualsSign()
+    {
+      var testItem = new BigMathNumber();
+      Assert.IsTrue(testItem == 0);
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberInstantiatesWithZeroValueByEqualsSignToAnotherBigMathNumber()
+    {
+      var testItem = new BigMathNumber();
+      Assert.IsTrue(testItem == 0);
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberInstantiatesWithZeroValueByEqualsMethod()
+    {
+      var testItem = new BigMathNumber();
+      Assert.IsTrue(testItem.Equals(0));
+    }
+
+    [TestMethod]
+    public void AssertBigMathNumberInstantiatesWithZeroValueByEqualsMethodToAnotherBigMathNumber()
+    {
+      var testItem = new BigMathNumber();
+      var testItem2 = new BigMathNumber();
+      Assert.IsTrue(testItem.Equals(testItem2));
+    }
+
+    [TestMethod]
+    public void BigMathNumberEqualsAnotherBigMathNumber()
+    {
+      var testItem = new BigMathNumber();
+      var testItem2 = new BigMathNumber();
+      testItem = 100;
+      testItem2 = 100;
+      Assert.IsTrue(testItem == testItem2);
+    }
+
+    [TestMethod]
+    public void BigMathNumberEqualsAnotherBigMathNumberTurnedIntoObject()
+    {
+      var testItem = new BigMathNumber();
+      var testItem2 = new BigMathNumber();
+      testItem = 100;
+      testItem2 = 100;
+      var newObject = testItem2 as object;
+      Assert.IsTrue(testItem.Equals(testItem2));
+    }
+
+    [TestMethod]
+    public void BigMathNumberNotEqualAnotherBigMathNumber()
+    {
+      var testItem = new BigMathNumber();
+      var testItem2 = new BigMathNumber();
+      testItem = 100;
+      testItem2 = 200;
+      Assert.IsTrue(testItem != testItem2);
+    }
+
+    [TestMethod]
+    public void BigMathNumberEqualAnotherBigMathNumberByEqualAssignmentOperator()
+    {
+      var testItem = new BigMathNumber();
+      var testItem2 = new BigMathNumber();
+      testItem = 100;
+      testItem2 = testItem;
+      Assert.IsTrue(testItem == testItem2);
+    }
+
+    [TestMethod]
+    public void BigMathNumberEqualsAnInt()
+    {
+      var testItem = new BigMathNumber();
+      testItem = 100;
+      int testItem2 = 100;
+      Assert.IsTrue(testItem == testItem2);
+    }
+
+    [TestMethod]
+    public void BigMathNumberNotEqualAnInt()
+    {
+      var testItem = new BigMathNumber();
+      testItem = 100;
+      int testItem2 = 200;
+      Assert.IsTrue(testItem != testItem2);
+    }
+
+
+  }
+}
---
+++ bigmath/BigMathNumber.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bigmath
+{
+  public class BigMathNumber :  IEquatable<BigMathNumber>
+  {
+    private List<sbyte> internalNumber;
+
+    public BigMathNumber()
+    {
+      Initialize();
+    }
+
+    private void Initialize()
+    {
+      internalNumber = new List<sbyte>();
+      sbyte newNumber = 0;
+      internalNumber.Add(newNumber); //init as zero
+    }
+
+    public static bool operator ==(BigMathNumber left, BigMathNumber right)
+    {
+      return left.Equals(right);
+    }
+
+    public static bool operator !=(BigMathNumber left, BigMathNumber right)
+    {
+      return !left.Equals(right);
+    }
+
+    public static bool operator ==(BigMathNumber left, int right)
+    {
+      return left.Equals(right);
+    }
+
+    public static bool operator !=(BigMathNumber left, int right)
+    {
+      return !left.Equals(right);
+    }
+
+    public static implicit operator BigMathNumber(int value)
+    {
+      var newBigNumber = new BigMathNumber();
+      newBigNumber.SetValue(value);
+      return newBigNumber;
+    }
+
+    public void SetValue(int value)
+    {
+      this.SetValue(value.ToString());
+    }
+
+    public void SetValue(BigMathNumber value)
+    {
+      this.SetValue(value.ToString());
+    }
+
+    public void SetValue(String value)
+    {
+      var newNumber = ConvertToList(value);
+      internalNumber = new List<sbyte>();
+      this.internalNumber.AddRange(newNumber);
+    }
+
+    private List<sbyte> ConvertToList(string value)
+    {
+      List<sbyte> newNumber = new List<sbyte>();
+      for (int i = value.Length - 1; i >= 0; i--)
+      {
+        newNumber.Add(Convert.ToSByte(value.Substring(i, 1)));
+      }
+      return newNumber;
+    }
+
+    public override string ToString()
+    {
+      //remove leading zeros, but not the last
+      while ((internalNumber.Last() == 0) && (internalNumber.Count > 1))
+      {
+        internalNumber.RemoveAt(internalNumber.Count - 1);
+      }
+
+      String retVal = String.Empty;
+      foreach (sbyte val in internalNumber)
+      {
+        retVal = Convert.ToString(val) + retVal;
+      }
+      return retVal;
+    }
+
+    public bool Equals(int other)
+    {
+      return AreEqual(this.ToString(), other.ToString());
+    }
+
+    public bool Equals(BigMathNumber other)
+    {
+      return AreEqual(this.ToString(), other.ToString());
+    }
+
+    public bool Equals(Object other)
+    {
+      try
+      {
+        var otherNumber = other as BigMathNumber;
+        return this.Equals(otherNumber);
+      }
+      catch (Exception)
+      {
+        return false;
+      }
+    }
+
+    private bool AreEqual(string left, string right)
+    {
+      return (left == right);
+    }
+
+    public int GetHasCode()
+    {
+      return internalNumber.GetHashCode();
+    }
+
+  }
+}
